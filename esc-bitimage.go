b77f18cdc06fb201257f3bdae34c8aa65dffe698
Homin Lee <homin.lee@suapapa.net>
2022-04-29 07:00:41 +0900

    24bit test done

--- esc-bitimage.go
+++ esc-bitimage.go
@@ -32,35 +32,41 @@ func printImage(wr io.Writer, fn string) error {
 	origW, origH := img.Bounds().Dx(), img.Bounds().Dy()
 
 	w := 512
-	h := ((origH * w) / origW) / 3
+	h := ((origH * w) / origW)
 	log.Println(w, h)
 	m := resize.Resize(uint(w), uint(h), img, resize.Lanczos3)
 
 	ditheredImg := dither.Monochrome(dither.Burkes, m, 1.18)
 	// rect := ditheredImg.Bounds()
 	// h := rect.Dy()
-	dataBuf := make([]byte, w*((h+7)/8))
+	dataBuf := make([]byte, (3*w*h+7)/8)
 
 	// 가로방향 점의 개수: nL + nH x 256
 	nH := byte(w / 256)
 	nL := byte(w % 256)
-	mode := byte(1)
+	mode := byte(33)
 	log.Println(nL, nH, mode)
 	cmdBuf := []byte{0x1B, 0x2A, mode, nL, nH}
 
 	dataBufIdx := 0
-	for y := 0; y < h; y += 8 {
+	for y := 0; y < h; y += 8 * 3 {
 		for x := 0; x < w; x++ {
 			var dataByte byte
-			for yi := 0; yi < 8; yi++ {
-				var bit byte
-				if r, g, b, _ := ditheredImg.At(x, y+yi).RGBA(); r == 0 && g == 0 && b == 0 {
-					bit = 1 << (7 - yi)
+			for yCnt := 0; yCnt < 3; yCnt++ {
+				for yi := 0; yi < 8; yi++ {
+					currY := y + (8 * yCnt) + yi
+					if currY > h {
+						continue
+					}
+					var bit byte
+					if r, g, b, _ := ditheredImg.At(x, currY).RGBA(); r == 0 && g == 0 && b == 0 {
+						bit = 1 << (7 - yi)
+					}
+					dataByte |= bit
 				}
-				dataByte |= bit
+				dataBuf[dataBufIdx] = dataByte
+				dataBufIdx += 1
 			}
-			dataBuf[dataBufIdx] = dataByte
-			dataBufIdx += 1
 		}
 	}
 	// log.Println(dataBuf)
@@ -71,8 +77,8 @@ func printImage(wr io.Writer, fn string) error {
 	// Line spacing
 	wr.Write([]byte{0x1B, 0x33, 0})
 
-	for i := 0; i < len(dataBuf); i += w {
-		printBuf := append(cmdBuf, dataBuf[i:i+w]...)
+	for i := 0; i < len(dataBuf); i += (w * 3) {
+		printBuf := append(cmdBuf, dataBuf[i:i+w*3]...)
 		wr.Write(printBuf)
 	}
 
